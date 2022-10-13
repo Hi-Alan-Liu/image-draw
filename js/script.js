dc122a1b5adc4c4c3961fb2664337676be68d135
pa013971 <pa013971@gmail.com>
2022-10-13 20:57:05 +0800

    排除 chrome 滑動問題

--- js/script.js
+++ js/script.js
@@ -21,6 +21,10 @@ let drawing = false;
 var min = 5, max = 15, select = document.getElementById('lineSize');
 var imageResult;
 
+window.addEventListener('touchstart', func, { passive: false });
+window.addEventListener('touchmove', func, { passive: false });
+window.addEventListener('touchend', func, { passive: false });
+
 for (var i = min; i <= max; i++) {
   var opt = document.createElement('option');
   opt.value = i;
